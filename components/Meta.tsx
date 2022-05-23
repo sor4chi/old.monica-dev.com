481fa9d01c3e61e9ab322f5758cedc10b3f30add
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-05-23 10:50:45 +0900

    Fix: literal

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -6,15 +6,13 @@ interface MetaProps {
 }
 
 export default function Meta({ title, description = "" }: MetaProps) {
-  const titleTemplate = (title: string) => {
-    return `${title} | Monica's Portfolio`;
-  };
+  const titleTemplate = `${title} | Monica's Portfolio`;
 
   return (
     <Head>
-      <title>{titleTemplate(title)}</title>
+      <title>{titleTemplate}</title>
       <meta property="description" content={description} />
-      <meta property="og:title" content={titleTemplate(title)} />
+      <meta property="og:title" content={titleTemplate} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={`ogp_large.png`} />
       <meta name="twitter:card" content="summary_large_image" />
