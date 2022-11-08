45b181cbddca7a521c0de3d9523a2af3f38eb10b
kawamurasorachi <sorakawa0218@gmail.com>
2022-11-08 13:55:57 +0900

    fix blog card design

--- ui/layout/header/logo.tsx
+++ ui/layout/header/logo.tsx
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export const Logo = () => (
-  <Link href="/" className='h-min'>
-    <h1 className="text-2xl font-bold text-neutral-800 dark:text-neutral-100">
-      Monica
-    </h1>
+  <Link href="/" className="h-min">
+    <div className="flex items-center space-x-2">
+      <Image src="/original.svg" alt="logo" width={40} height={40} />
+      <h1 className="text-2xl font-bold text-neutral-800 dark:text-neutral-100">
+        Monica
+      </h1>
+    </div>
   </Link>
 );
