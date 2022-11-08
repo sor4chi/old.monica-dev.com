5c857a200edbdac81b810fd9e3c4ed706d8ba52a
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-11-08 10:30:03 +0900

    Fix: card design

--- ui/blog/card.tsx
+++ ui/blog/card.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import Link from 'next/link';
 
 import { Blog } from '#/types/blog';
@@ -13,12 +14,18 @@ export const BlogCard = ({ blog }: Props) => {
   return (
     <Link
       href={blog.url || `blogs/${blog.slug}` || ''}
-      className="rounded-lg dark:text-white text-neutral-800 flex flex-col space-y-2 dark:hover:bg-neutral-800 hover:bg-slate-200 p-4"
+      className={clsx(
+        'border-2 rounded-lg flex flex-col space-y-2 p-4',
+        'text-neutral-800 bg-slate-50 border-white',
+        'dark:text-slate-200 dark:bg-neutral-800 dark:border-neutral-700',
+      )}
     >
-      <div className="text-2xl font-bold">{blog.title}</div>
+      <div className="text-xl font-bold">{blog.title}</div>
       <div className="flex justify-between items-center">
         <BlogTagList tags={blog.tags} />
-        <div className="text-sm">{dateToPassedTimeByNow(blog.date)}</div>
+        <div className="text-sm text-neutral-400 dark:text-neutral-500">
+          {dateToPassedTimeByNow(blog.date)}
+        </div>
       </div>
     </Link>
   );
