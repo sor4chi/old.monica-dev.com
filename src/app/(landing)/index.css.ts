1eb25a8acc7b73371cde318d5e628602a60a24ed
sor4chi <sorakawa0218@gmail.com>
2023-03-07 04:22:27 +0900

    feat: design top page

--- src/app/(landing)/index.css.ts
+++ src/app/(landing)/index.css.ts
@@ -1,15 +1,108 @@
 import { vars } from "@/style/theme.css";
-import { style } from "@vanilla-extract/css";
+import { keyframes, style } from "@vanilla-extract/css";
 
 export const container = style({
-  maxWidth: "1024px",
-  margin: "0 auto",
+  position: "relative",
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
 });
 
+export const themeSwitchContainer = style({
+  position: "absolute",
+  top: "1rem",
+  right: "1rem",
+});
+
+export const icon = style({
+  borderRadius: "50%",
+  boxShadow: `0 0 1rem 0 ${vars.color.bg.secondary}`,
+});
+
+const titleLineHeight = "3.5rem";
+const titleFontSize = "3rem";
+
 export const title = style({
-  fontSize: "2rem",
+  fontSize: titleFontSize,
+  lineHeight: titleLineHeight,
   fontWeight: 700,
   textAlign: "center",
   color: vars.color.text.primary,
-  margin: "2rem 0",
+  display: "inline-flex",
+  alignItems: "center",
+  overflow: "hidden",
+  margin: 0,
+  marginTop: "2rem",
+});
+
+const stopTime = 10;
+const moveTime = 1;
+const total = stopTime * 2 + moveTime * 2;
+const stopTimePercent = (stopTime / total) * 100;
+const moveTimePercent = (moveTime / total) * 100;
+const steps = [
+  "0%",
+  `${stopTimePercent / 2}%`,
+  `${stopTimePercent / 2 + moveTimePercent}%`,
+  `${stopTimePercent / 2 + moveTimePercent + stopTimePercent}%`,
+  `${stopTimePercent / 2 + moveTimePercent * 2 + stopTimePercent}%`,
+  "100%",
+] as const;
+
+const toggleAnimation = keyframes({
+  [steps[0]]: { transform: "translateY(0)" },
+  [steps[1]]: { transform: "translateY(0)" },
+  [steps[2]]: { transform: `translateY(-${titleLineHeight})` },
+  [steps[3]]: { transform: `translateY(-${titleLineHeight})` },
+  [steps[4]]: { transform: "translateY(0)" },
+  [steps[5]]: { transform: "translateY(0)" },
+});
+
+export const titleToggle = style({
+  display: "inline-flex",
+  flexDirection: "column",
+  height: titleLineHeight,
+  marginRight: "1rem",
+
+  animationName: toggleAnimation,
+  animationDelay: `${stopTime / 2}s`,
+  animationDuration: `${total}s`,
+  animationIterationCount: "infinite",
+  animationTimingFunction: "ease-in-out",
+});
+
+export const subtitle = style({
+  fontSize: "1.5rem",
+  lineHeight: "2rem",
+  fontWeight: 400,
+  textAlign: "center",
+  color: vars.color.text.secondary,
+  margin: 0,
+  marginTop: "1rem",
+});
+
+export const social = style({
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "1rem",
+  padding: 0,
+  margin: 0,
+  marginTop: "1rem",
+});
+
+export const socialItem = style({
+  listStyle: "none",
+});
+
+export const socialLink = style({
+  color: vars.color.text.secondary,
+
+  ":hover": {
+    opacity: 0.8,
+  },
 });
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,52 @@
 import * as styles from "./index.css";
+import { FaGithub, FaTwitter } from "react-icons/fa";
+import Link from "next/link";
+import Image from "next/image";
+import { ThemeSwitch } from "@/ui/foundation/navigator/themeSwitch";
 
 export default function Home() {
   return (
     <article className={styles.container}>
-      <h1 className={styles.title}>Hello World</h1>
+      <div className={styles.themeSwitchContainer}>
+        <ThemeSwitch />
+      </div>
+      <Image
+        src="/icon.webp"
+        alt="Monica / Sor4chi"
+        width={240}
+        height={240}
+        className={styles.icon}
+      />
+      <h1 className={styles.title}>
+        <span className={styles.titleToggle}>
+          <span>Sor4chi</span>
+          <span>Monica</span>
+        </span>
+        <span style={{ display: "inline" }}>Portfolio</span>
+      </h1>
+      <p className={styles.subtitle}>Web App Developer</p>
+      <ul className={styles.social}>
+        <li className={styles.socialItem}>
+          <Link
+            href="https://twitter.com/monica18_pr"
+            target="_blank"
+            className={styles.socialLink}
+            passHref
+          >
+            <FaTwitter size="2rem" />
+          </Link>
+        </li>
+        <li className={styles.socialItem}>
+          <Link
+            href="https://github.com/sor4chi"
+            target="_blank"
+            className={styles.socialLink}
+            passHref
+          >
+            <FaGithub size="2rem" />
+          </Link>
+        </li>
+      </ul>
     </article>
   );
 }
