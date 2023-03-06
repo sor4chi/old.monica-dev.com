eb2a67f179e6b298751c4c5b2ea542e13cd0cd9e
sor4chi <sorakawa0218@gmail.com>
2023-03-07 04:47:05 +0900

    chore: configure prettier + eslint

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'plugin:import/recommended',
+    'plugin:sort/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['import', 'unused-imports', 'sort', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-imports': ['error'],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'import/namespace': ['off'],
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    'sort/imports': ['off'],
+    'unused-imports/no-unused-imports': 'warn',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.cts', '.mts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+};
