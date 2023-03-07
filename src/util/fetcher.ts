12e2e5f996cf4c95a32895d43090ac788d30980d
sor4chi <sorakawa0218@gmail.com>
2023-03-07 14:39:55 +0900

    feat: create custom fetcher

---
+++ src/util/fetcher.ts
@@ -0,0 +1,30 @@
+const wrap = <T>(task: Promise<Response>): Promise<T> => {
+  return new Promise((resolve, reject) => {
+    task
+      .then((response) => {
+        if (response.ok) {
+          response
+            .json()
+            .then((json) => {
+              // jsonが取得できた場合だけresolve
+              resolve(json);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        } else {
+          reject(response);
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+export const customFetch = <T>(input: RequestInfo, init?: RequestInit | undefined): Promise<T> => {
+  if (typeof input === 'string' && !/^https?:\/\//.test(input)) {
+    input = new URL(input, 'http://localhost:3000').toString();
+  }
+  return wrap<T>(fetch(input, init));
+};
