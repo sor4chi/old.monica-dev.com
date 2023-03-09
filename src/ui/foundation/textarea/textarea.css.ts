09fa038dae49aa408438361ecacaf6cdfb8e9870
sor4chi <sorakawa0218@gmail.com>
2023-03-10 08:23:25 +0900

    feat: error for text area

---
+++ src/ui/foundation/textarea/textarea.css.ts
@@ -0,0 +1,38 @@
+import { style } from '@vanilla-extract/css';
+
+import { vars } from '@/style/theme.css';
+
+export const wrapper = style({
+  width: '100%',
+});
+
+export const label = style({
+  color: vars.color.text.secondary,
+  margin: '0 0 0.5rem',
+  display: 'inline-block',
+});
+
+export const textarea = style({
+  width: '100%',
+  boxSizing: 'border-box',
+  padding: '0.5rem 1rem',
+  border: 'none',
+  borderRadius: '0.25rem',
+  fontSize: '1rem',
+  lineHeight: '1.5',
+  color: vars.color.text.primary,
+  backgroundColor: vars.color.bg.secondary,
+  backgroundClip: 'padding-box',
+  transition: 'border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out',
+
+  ':focus': {
+    borderColor: vars.color.accent.primary,
+    outline: 'none',
+    boxShadow: `0 0 0 0.2rem ${vars.color.accent.primary}`,
+  },
+});
+
+export const error = style({
+  color: vars.color.error,
+  margin: '0.5rem 0 0',
+});
