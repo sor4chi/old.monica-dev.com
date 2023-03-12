c0d332f94307b7b2938bef6a69121e481a49589e
sor4chi <sorakawa0218@gmail.com>
2023-03-13 01:01:20 +0900

    chore: deactivate blog api route

--- src/app/api/blog/[slug]/route.ts
+++ src/app/api/blog/[slug]/route.ts
@@ -1,68 +1,68 @@
-import { blogData, blogsData } from '../data';
+// import { blogData, blogsData } from '../data';
 
-import { parseMarkdownToHTML } from '@/lib/markdown';
-import { customFetch } from '@/util/fetcher';
+// import { parseMarkdownToHTML } from '@/lib/markdown';
+// import { customFetch } from '@/util/fetcher';
 
-interface TocItem {
-  /** ヘッダーのレベル */
-  depth: number;
-  /** ヘッダーのテキスト */
-  value: string;
-  /** ヘッダーの属性データ */
-  data: {
-    id: string;
-  };
-  /** ヘッダーの子要素 */
-  children: TocItem[];
-}
+// interface TocItem {
+//   /** ヘッダーのレベル */
+//   depth: number;
+//   /** ヘッダーのテキスト */
+//   value: string;
+//   /** ヘッダーの属性データ */
+//   data: {
+//     id: string;
+//   };
+//   /** ヘッダーの子要素 */
+//   children: TocItem[];
+// }
 
-type BlogShowResponse = {
-  /** ブログのID */
-  id: string;
-  /** ブログのSlug */
-  slug: string;
-  /** ブログのタイトル */
-  title: string;
-  /** ブログの説明 */
-  description: string;
-  /** ブログの本文 */
-  content: string;
-  /** ブログのTOC */
-  toc: TocItem[];
-  /** ブログのタグ */
-  tags: string[];
-  /** ブログの作成日 */
-  createdAt: string;
-  /** ブログの更新日 */
-  updatedAt: string;
-};
+// type BlogShowResponse = {
+//   /** ブログのID */
+//   id: string;
+//   /** ブログのSlug */
+//   slug: string;
+//   /** ブログのタイトル */
+//   title: string;
+//   /** ブログの説明 */
+//   description: string;
+//   /** ブログの本文 */
+//   content: string;
+//   /** ブログのTOC */
+//   toc: TocItem[];
+//   /** ブログのタグ */
+//   tags: string[];
+//   /** ブログの作成日 */
+//   createdAt: string;
+//   /** ブログの更新日 */
+//   updatedAt: string;
+// };
 
-export async function GET(_request: Request, { params }: { params: { slug: string } }) {
-  const { slug } = params;
-  const data = blogsData.find((blog) => blog.slug === slug);
-  if (!data) {
-    return new Response('Not Found', {
-      headers: {
-        'content-type': 'text/plain',
-      },
-      status: 404,
-    });
-  }
+// export async function GET(_request: Request, { params }: { params: { slug: string } }) {
+//   const { slug } = params;
+//   const data = blogsData.find((blog) => blog.slug === slug);
+//   if (!data) {
+//     return new Response('Not Found', {
+//       headers: {
+//         'content-type': 'text/plain',
+//       },
+//       status: 404,
+//     });
+//   }
 
-  const parsed = parseMarkdownToHTML(data.content || blogData);
+//   const parsed = parseMarkdownToHTML(data.content || blogData);
 
-  const response = {
-    ...data,
-    ...parsed,
-  };
+//   const response = {
+//     ...data,
+//     ...parsed,
+//   };
 
-  return new Response(JSON.stringify(response), {
-    headers: {
-      'content-type': 'application/json',
-    },
-  });
-}
+//   return new Response(JSON.stringify(response), {
+//     headers: {
+//       'content-type': 'application/json',
+//     },
+//   });
+// }
 
-export const fetchGetBlogBySlug = (slug: string) => {
-  return customFetch<BlogShowResponse>(`/api/blog/${slug}`);
-};
+// export const fetchGetBlogBySlug = (slug: string) => {
+//   return customFetch<BlogShowResponse>(`/api/blog/${slug}`);
+// };
--- src/app/api/blog/slugs/route.ts
+++ src/app/api/blog/slugs/route.ts
@@ -1,19 +1,19 @@
-import { blogsData } from './../data';
+// import { blogsData } from '../data';
 
-import { customFetch } from '@/util/fetcher';
+// import { customFetch } from '@/util/fetcher';
 
-type BlogGetSlugsResponse = string[];
+// type BlogGetSlugsResponse = string[];
 
-export async function GET(_request: Request) {
-  const response = blogsData.map((blog) => blog.slug) satisfies BlogGetSlugsResponse;
+// export async function GET(_request: Request) {
+//   const response = blogsData.map((blog) => blog.slug) satisfies BlogGetSlugsResponse;
 
-  return new Response(JSON.stringify(response), {
-    headers: {
-      'content-type': 'application/json',
-    },
-  });
-}
+//   return new Response(JSON.stringify(response), {
+//     headers: {
+//       'content-type': 'application/json',
+//     },
+//   });
+// }
 
-export const fetchGetBlogSlugs = () => {
-  return customFetch<BlogGetSlugsResponse>(`/api/blog/slugs`);
-};
+// export const fetchGetBlogSlugs = () => {
+//   return customFetch<BlogGetSlugsResponse>(`/api/blog/slugs`);
+// };
