33729679d2162b5dbe6356e979b5fde3b3f74aec
sor4chi <sorakawa0218@gmail.com>
2023-03-13 03:36:53 +0900

    feat: about page placeholder

--- src/app/(portfoilo)/about/about.css.ts
+++ src/app/(portfoilo)/about/about.css.ts
@@ -1,11 +1,26 @@
-import { style } from '@vanilla-extract/css';
+import { globalStyle, style } from '@vanilla-extract/css';
 
 import { vars } from '@/style/theme.css';
 
 export const title = style({
   color: vars.color.text.primary,
   fontSize: '2rem',
-  fontWeight: 700,
+  fontWeight: 300,
   margin: '2rem 0',
   textAlign: 'center',
+});
+
+export const text = style({
+  color: vars.color.text.secondary,
+  fontSize: '1.25rem',
+  fontWeight: 300,
+  margin: '2rem 0',
+  textAlign: 'center',
+});
+
+globalStyle(`${text} .twemoji`, {
+  width: '1em',
+  height: '1em',
+  verticalAlign: '-0.1em',
+  margin: '0 0.25rem',
 });
--- src/app/(portfoilo)/about/page.tsx
+++ src/app/(portfoilo)/about/page.tsx
@@ -1,9 +1,21 @@
+import twemoji from 'twemoji';
+
 import * as styles from './about.css';
 
 export default function About() {
   return (
     <>
-      <h1 className={styles.title}>About Page</h1>
+      <h1 className={styles.title}>About</h1>
+      <p
+        className={styles.text}
+        dangerouslySetInnerHTML={{
+          __html: twemoji.parse('⚠️Now Preparing⚠️', {
+            className: 'twemoji',
+            ext: '.svg',
+            folder: 'svg',
+          }),
+        }}
+      />
     </>
   );
 }
