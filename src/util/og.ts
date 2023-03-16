e6abc8215fd141da57d009be4f739f27491dceb2
sor4chi <sorakawa0218@gmail.com>
2023-03-16 11:33:02 +0900

    chore: update ogp-url

--- src/util/og.ts
+++ src/util/og.ts
@@ -1,3 +1,3 @@
 export const getOgUrl = (text: string) => {
-  return `https://res.cloudinary.com/dj8lujsue/image/upload/l_text:Sawarabi%20Gothic_72_light:${text},co_rgb:fff,w_1000,c_fit/v1638148802/background_jisyrc.png`;
+  return `https://res.cloudinary.com/dj8lujsue/image/upload/l_text:Sawarabi%20Gothic_72_bold:${text},co_rgb:fff,w_1000,c_fit/v1638148802/ogp_u7qqng.png`;
 };
