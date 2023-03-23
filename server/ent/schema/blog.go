faf3e53137e51bade0f21f99db49f8a9165fdf27
sor4chi <sorakawa0218@gmail.com>
2023-03-24 08:20:47 +0900

    feat(server): add blog schema filed

--- server/ent/schema/blog.go
+++ server/ent/schema/blog.go
@@ -1,6 +1,9 @@
 package schema
 
 import (
+	"regexp"
+	"time"
+
 	"entgo.io/contrib/entgql"
 	"entgo.io/ent"
 	"entgo.io/ent/schema"
@@ -17,8 +20,53 @@ func (Blog) Fields() []ent.Field {
 	return []ent.Field{
 		field.String("title").
 			NotEmpty().
+			Comment("ブログのタイトル").
+			MaxLen(64).
 			Annotations(
 				entgql.OrderField("TITLE"),
+			),
+		field.String("slug").
+			NotEmpty().
+			Unique().
+			Match(regexp.MustCompile(`^[a-z0-9]+(?:-[a-z0-9]+)*$`)).
+			MaxLen(64).
+			Comment("ブログのスラッグ、URLのパラメータとして使用").
+			Annotations(
+				entgql.OrderField("SLUG"),
+			),
+		field.String("description").
+			NotEmpty().
+			Comment("ブログの説明").
+			MaxLen(256).
+			Annotations(
+				entgql.OrderField("DESCRIPTION"),
+			),
+		field.String("content").
+			NotEmpty().
+			Comment("ブログの本文").
+			Annotations(
+				entgql.OrderField("CONTENT"),
+			),
+		field.Time("created_at").
+			Default(time.Now).
+			Immutable().
+			Comment("ブログの作成日時").
+			Annotations(
+				entgql.OrderField("CREATED_AT"),
+			),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now).
+			Comment("ブログの更新日時").
+			Annotations(
+				entgql.OrderField("UPDATED_AT"),
+			),
+		field.Time("published_at").
+			Optional().
+			Nillable().
+			Comment("ブログの公開日時、公開されていない場合はnull").
+			Annotations(
+				entgql.OrderField("PUBLISHED_AT"),
 			),
 	}
 }
