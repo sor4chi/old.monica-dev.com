9fbe8c8e556119b8fe4b5ab414f9230abe5ae6c7
sor4chi <sorakawa0218@gmail.com>
2023-03-24 08:01:58 +0900

    fix(server): add go container for development, connect to mysql container

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"entgo.io/ent/dialect"
 	"github.com/99designs/gqlgen/graphql/handler"
@@ -12,12 +14,43 @@ import (
 	"github.com/sor4chi/portfolio-blog/server/ent"
 	"github.com/sor4chi/portfolio-blog/server/ent/migrate"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type MySQLConnectionEnv struct {
+	Host     string
+	Port     string
+	User     string
+	DBName   string
+	Password string
+}
+
+func getEnv(key string, defaultValue string) string {
+	val := os.Getenv(key)
+	if val != "" {
+		return val
+	}
+	return defaultValue
+}
+
+func NewMySQLConnectionEnv() *MySQLConnectionEnv {
+	return &MySQLConnectionEnv{
+		Host:     getEnv("MYSQL_HOST", "mysql"),
+		Port:     getEnv("MYSQL_PORT", "3306"),
+		User:     getEnv("MYSQL_USER", "monica"),
+		DBName:   getEnv("MYSQL_DATABASE", "portfolio"),
+		Password: getEnv("MYSQL_PASSWORD", "dev"),
+	}
+}
+
+func dsn(env *MySQLConnectionEnv) string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True",
+		env.User, env.Password, env.Host, env.Port, env.DBName)
+}
 
 func main() {
 	// Create ent.Client and run the schema migration.
-	client, err := ent.Open(dialect.SQLite, "file:ent?mode=memory&cache=shared&_fk=1")
+	client, err := ent.Open(dialect.MySQL, dsn(NewMySQLConnectionEnv()))
 	if err != nil {
 		log.Fatal("opening ent client", err)
 	}
--- server/go.mod
+++ server/go.mod
@@ -3,7 +3,7 @@ module github.com/sor4chi/portfolio-blog/server
 go 1.19
 
 require (
-	entgo.io/contrib v0.3.6-0.20230320072633-f0e9994c3a59
+	entgo.io/contrib v0.3.5
 	entgo.io/ent v0.11.10
 	github.com/99designs/gqlgen v0.17.27
 	github.com/hashicorp/go-multierror v1.1.1
@@ -17,19 +17,16 @@ require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/go-openapi/inflect v0.19.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.1 // indirect
 	github.com/hashicorp/hcl/v2 v2.13.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/vmihailenco/msgpack/v5 v5.0.0-beta.9 // indirect
-	github.com/vmihailenco/tagparser v0.1.2 // indirect
 	github.com/zclconf/go-cty v1.8.0 // indirect
-	golang.org/x/exp v0.0.0-20221230185412-738e83a70c30 // indirect
 	golang.org/x/mod v0.9.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
