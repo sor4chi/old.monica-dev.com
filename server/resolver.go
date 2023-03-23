96e6cff02939e55db4eddeb7347697fea18801e5
sor4chi <sorakawa0218@gmail.com>
2023-03-24 07:11:17 +0900

    feat(server): use ent.Client as graphql resolver

--- server/resolver.go
+++ server/resolver.go
@@ -1,7 +1,14 @@
 package main
 
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require here.
+import (
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/sor4chi/portfolio-blog/server/ent"
+)
 
-type Resolver struct{}
+type Resolver struct{ client *ent.Client }
+
+func NewSchema(client *ent.Client) graphql.ExecutableSchema {
+	return NewExecutableSchema(Config{
+		Resolvers: &Resolver{client},
+	})
+}
