ff17cb5d752912928f459fe36fd98efb1fecce06
sor4chi <sorakawa0218@gmail.com>
2023-03-24 07:18:36 +0900

    feat(server): configure server launch cmd

---
+++ server/cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"entgo.io/ent/dialect"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	server "github.com/sor4chi/portfolio-blog/server"
+	"github.com/sor4chi/portfolio-blog/server/ent"
+	"github.com/sor4chi/portfolio-blog/server/ent/migrate"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	// Create ent.Client and run the schema migration.
+	client, err := ent.Open(dialect.SQLite, "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatal("opening ent client", err)
+	}
+	if err := client.Schema.Create(
+		context.Background(),
+		migrate.WithGlobalUniqueID(true),
+	); err != nil {
+		log.Fatal("opening ent client", err)
+	}
+
+	// Configure the server and start listening on :8081.
+	srv := handler.NewDefaultServer(server.NewSchema(client))
+	http.Handle("/",
+		playground.Handler("Blog", "/query"),
+	)
+	http.Handle("/query", srv)
+	log.Println("listening on :8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal("http server terminated", err)
+	}
+}
