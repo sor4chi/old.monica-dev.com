202ae61efbbb095806af0fc99b0e51cbde8651d2
sor4chi <sorakawa0218@gmail.com>
2023-03-29 10:07:33 +0900

    fix(server): update blog scheme, not null

--- server/entity/blog.go
+++ server/entity/blog.go
@@ -8,13 +8,13 @@ import (
 
 type Blog struct {
 	gorm.Model
-	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	Slug        string    `json:"slug" gorm:"type:varchar(255);uniqueIndex:idx_blog_slug"`
-	Description string    `json:"description"`
-	Content     string    `json:"content"`
+	ID          int       `json:"id" gorm:"not null"`
+	Title       string    `json:"title" gorm:"not null"`
+	Slug        string    `json:"slug" gorm:"type:varchar(255);uniqueIndex:idx_blog_slug;not null"`
+	Description string    `json:"description" gorm:"not null"`
+	Content     string    `json:"content" gorm:"not null"`
 	Tags        []*Tag    `json:"tags" gorm:"many2many:blog_tags;"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	CreatedAt   time.Time `json:"created_at" gorm:"not null"`
+	UpdatedAt   time.Time `json:"updated_at" gorm:"not null"`
 	PublishedAt time.Time `json:"published_at"`
 }
--- server/entity/tag.go
+++ server/entity/tag.go
@@ -9,9 +9,9 @@ import (
 type Tag struct {
 	gorm.Model
 	ID        int       `json:"id"`
-	Slug      string    `json:"slug" gorm:"type:varchar(255);uniqueIndex:idx_tag_slug"`
-	Name      string    `json:"name"`
+	Slug      string    `json:"slug" gorm:"type:varchar(255);uniqueIndex:idx_tag_slug;not null"`
+	Name      string    `json:"name" gorm:"not null"`
 	Blogs     []*Blog   `json:"blogs" gorm:"many2many:blog_tags;"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at" gorm:"not null"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"not null"`
 }
