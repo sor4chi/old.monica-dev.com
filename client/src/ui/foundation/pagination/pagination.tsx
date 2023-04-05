49cedb486f55d39227754a51c3bfd5f56e0906b9
sor4chi <sorakawa0218@gmail.com>
2023-04-05 22:27:39 +0900

    fix(client): if max-page less than 1, suppress pagination rendering

--- client/src/ui/foundation/pagination/pagination.tsx
+++ client/src/ui/foundation/pagination/pagination.tsx
@@ -11,22 +11,26 @@ interface Props {
   loadAfter: () => void;
 }
 
-export const Pagination = ({ loadAfter, loadBefore, maxPage, page }: Props) => (
-  <div className={styles.pagination}>
-    {page > 1 && (
-      <Button onClick={loadBefore} variant="secondary" icon={<MdArrowBackIos />} iconPosition="left">
-        Prev
-      </Button>
-    )}
-    {page > 1 && (
-      <span className={styles.page}>
-        {page} / {maxPage}
-      </span>
-    )}
-    {page < maxPage && (
-      <Button onClick={loadAfter} variant="secondary" icon={<MdArrowForwardIos />} iconPosition="right">
-        Next
-      </Button>
-    )}
-  </div>
-);
+export const Pagination = ({ loadAfter, loadBefore, maxPage, page }: Props) => {
+  if (maxPage < 1) return null;
+
+  return (
+    <div className={styles.pagination}>
+      {page > 1 && (
+        <Button onClick={loadBefore} variant="secondary" icon={<MdArrowBackIos />} iconPosition="left">
+          Prev
+        </Button>
+      )}
+      {page > 1 && (
+        <span className={styles.page}>
+          {page} / {maxPage}
+        </span>
+      )}
+      {page < maxPage && (
+        <Button onClick={loadAfter} variant="secondary" icon={<MdArrowForwardIos />} iconPosition="right">
+          Next
+        </Button>
+      )}
+    </div>
+  );
+};
