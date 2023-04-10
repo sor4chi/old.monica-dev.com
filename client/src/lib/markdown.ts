a090d05dff58e88433915cce124f6aff05b2ed54
sor4chi <sorakawa0218@gmail.com>
2023-04-10 16:15:38 +0900

    perf(client): cache processor out of rendering

--- client/src/lib/markdown.ts
+++ client/src/lib/markdown.ts
@@ -56,28 +56,28 @@ interface TocItem {
   children: TocItem[];
 }
 
+const mdHtmlProcessor = unified()
+  .use(remarkParse) // [md    -> mdast] Markdownをmdast(Markdown抽象構文木)に変換
+  .use(remarkSlug) // [mdast -> mdast] Headingにid付与（Toc Anchor用）
+  .use(remarkGfm) // [mdast -> mdast] table等の拡張md記法変換
+  .use(remarkMath) // [mdast -> mdast] mathブロックを変換
+  .use(remarkDirective) // [mdast -> mdast] messageブロックを変換
+  .use(remarkCodeTitle) // [mdast -> mdast] codeブロックへタイトル等の構文拡張
+  .use(remarkRehype) // [mdast -> hast ] mdast(Markdown抽象構文木)をhast(HTML抽象構文木)に変換
+  .use(rehypeKatex) // [mdast -> hast ] mathブロックをkatex.jsに対応
+  .use(rehypePrism, {
+    ignoreMissing: true,
+  })
+  .use(rehypeStringify); // [hast  -> html ] hast(HTML抽象構文木)をHTMLに変換
+
+const tocProcessor = unified()
+  .use(remarkParse) // [md    -> mdast] Markdownをmdast(Markdown抽象構文木)に変換
+  .use(remarkSlug) // [mdast -> mdast] Headingにid付与（Toc Anchor用）
+  .use(remarkExtractToc, {
+    keys: ['data'],
+  });
+
 export const parseMarkdownToHTML = (mdContent: string) => {
-  const mdHtmlProcessor = unified()
-    .use(remarkParse) // [md    -> mdast] Markdownをmdast(Markdown抽象構文木)に変換
-    .use(remarkSlug) // [mdast -> mdast] Headingにid付与（Toc Anchor用）
-    .use(remarkGfm) // [mdast -> mdast] table等の拡張md記法変換
-    .use(remarkMath) // [mdast -> mdast] mathブロックを変換
-    .use(remarkDirective) // [mdast -> mdast] messageブロックを変換
-    .use(remarkCodeTitle) // [mdast -> mdast] codeブロックへタイトル等の構文拡張
-    .use(remarkRehype) // [mdast -> hast ] mdast(Markdown抽象構文木)をhast(HTML抽象構文木)に変換
-    .use(rehypeKatex) // [mdast -> hast ] mathブロックをkatex.jsに対応
-    .use(rehypePrism, {
-      ignoreMissing: true,
-    })
-    .use(rehypeStringify); // [hast  -> html ] hast(HTML抽象構文木)をHTMLに変換
-
-  const tocProcessor = unified()
-    .use(remarkParse) // [md    -> mdast] Markdownをmdast(Markdown抽象構文木)に変換
-    .use(remarkSlug) // [mdast -> mdast] Headingにid付与（Toc Anchor用）
-    .use(remarkExtractToc, {
-      keys: ['data'],
-    });
-
   return {
     content: mdHtmlProcessor.processSync(mdContent).toString(),
     toc: tocProcessor.runSync(tocProcessor.parse(mdContent)) as unknown as TocItem[],
