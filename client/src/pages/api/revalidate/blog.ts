a65886bb4d9c559ca5f88bce6bc63ad397327d42
sor4chi <sorakawa0218@gmail.com>
2023-04-10 13:52:05 +0900

    feat(client): add revalidate stack trace

--- client/src/pages/api/revalidate/blog.ts
+++ client/src/pages/api/revalidate/blog.ts
@@ -2,6 +2,8 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { serverEnv } from '@/env/server';
 
+const PATH = (slug: string) => `/blog/${slug}`;
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.query.secret !== serverEnv.REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid token' });
@@ -9,12 +11,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const slug = req.query.slug as string;
 
-  console.log(`Revalidating: ${slug}`);
-
   try {
-    await res.revalidate(`/blog/${slug}`);
+    console.log(`Revalidating: ${PATH(slug)}`);
+    await res.revalidate(PATH(slug));
     return res.json({ revalidated: true });
   } catch (err) {
+    console.error(err);
     return res.status(500).send('Error revalidating');
   }
 }
