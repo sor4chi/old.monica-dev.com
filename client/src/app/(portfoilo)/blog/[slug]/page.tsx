173d5e6c4cbd791990863eed0a2abb77d827dafd
sor4chi <sorakawa0218@gmail.com>
2023-04-11 13:37:20 +0900

    fix(client): isr setting

--- client/src/app/(portfoilo)/blog/[slug]/page.tsx
+++ client/src/app/(portfoilo)/blog/[slug]/page.tsx
@@ -17,8 +17,8 @@ interface Props {
 }
 
 // force-static for ISR, because static page for SEO
-export const dynamic = 'force-static';
-export const revalidate = 'force-cache';
+export const dynamic = 'force-static',
+  dynamicParams = true;
 
 const BlogDetailPageQuery = gql`
   ${BlogHeroFragment}
