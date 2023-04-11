623d3cc4ce68d12ab1af6ebbd6157bf493f8f752
sor4chi <sorakawa0218@gmail.com>
2023-04-11 13:43:15 +0900

    fix(client): update caching with graphql client

--- client/src/lib/graphql.ts
+++ client/src/lib/graphql.ts
@@ -6,20 +6,8 @@ export { gql } from 'graphql-request';
 
 export const client = new GraphQLClient(clientEnv.NEXT_PUBLIC_SERVER_GQL_ENDPOINT, {
   credentials: 'include',
-  fetch: (url: string, options: RequestInit) => {
-    return fetch(url, {
-      ...options,
-      cache: 'no-cache',
-    });
-  },
 });
 
 export const clientInBrowser = new GraphQLClient(clientEnv.NEXT_PUBLIC_GQL_ENDPOINT, {
   credentials: 'include',
-  fetch: (url: string, options: RequestInit) => {
-    return fetch(url, {
-      ...options,
-      cache: 'no-cache',
-    });
-  },
 });
