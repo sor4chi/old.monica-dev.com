fe9d8b9864bfe7d01d70d56bec3c1ccfa6ed8ff8
sor4chi <sorakawa0218@gmail.com>
2023-04-24 13:35:01 +0900

    feat(ui): Add timeline component with styles and data

---
+++ client/src/ui/feature/about/timeline/timeline.css.ts
@@ -0,0 +1,16 @@
+import { style } from '@vanilla-extract/css';
+
+export const wrapper = style({
+  maxWidth: '40rem',
+  width: '100%',
+  margin: '0 auto',
+  padding: '0 1rem',
+});
+
+export const section = style({
+  margin: '2rem 0',
+});
+
+export const year = style({
+  margin: '0 0 .5rem',
+});
--- client/src/ui/feature/about/timeline/timeline.tsx
+++ client/src/ui/feature/about/timeline/timeline.tsx
@@ -1,3 +1,113 @@
+import * as styles from './timeline.css';
+
+import { TimelineList } from '@/ui/foundation/timeline';
+
+const TABS = [
+  {
+    blog: {
+      slug: 'blog-title',
+      title: 'これは技術ブログのタイトルです',
+    },
+    category: 'unknown',
+    date: '2021-01-01',
+    title: 'Timeline List Title',
+  },
+  {
+    category: 'award',
+    date: '2021-01-01',
+    title: 'Timeline List Title',
+  },
+  {
+    blog: {
+      slug: 'blog-title',
+      title: 'これは技術ブログのタイトルです',
+    },
+    category: 'blog',
+    date: '2021-01-01',
+    title: 'Timeline List Title',
+  },
+  {
+    category: 'education',
+    date: '2021-01-01',
+    title: 'Timeline List Title',
+  },
+  {
+    blog: {
+      slug: 'blog-title',
+      title: 'これは技術ブログのタイトルです',
+    },
+    category: 'product',
+    date: '2021-01-01',
+    title: 'Timeline List Title',
+  },
+  {
+    category: 'work',
+    date: '2021-01-01',
+    title: 'Timeline List Title',
+  },
+  {
+    blog: {
+      slug: 'blog-title',
+      title: 'これは技術ブログのタイトルです',
+    },
+    category: 'unknown',
+    date: '2022-01-01',
+    title: 'Timeline List Title',
+  },
+  {
+    category: 'award',
+    date: '2022-01-01',
+    title: 'Timeline List Title',
+  },
+  {
+    blog: {
+      slug: 'blog-title',
+      title: 'これは技術ブログのタイトルです',
+    },
+    category: 'blog',
+    date: '2022-01-01',
+    title: 'Timeline List Title',
+  },
+  {
+    category: 'education',
+    date: '2022-01-01',
+    title: 'Timeline List Title',
+  },
+  {
+    blog: {
+      slug: 'blog-title',
+      title: 'これは技術ブログのタイトルです',
+    },
+    category: 'product',
+    date: '2022-01-01',
+    title: 'Timeline List Title',
+  },
+  {
+    category: 'work',
+    date: '2022-01-01',
+    title: 'Timeline List Title',
+  },
+];
+
 export const Timeline = () => {
-  return <div className="timeline">aaa</div>;
+  const tabsSplitByYear = TABS.reduce((acc, cur) => {
+    const year = new Date(cur.date).getFullYear();
+    if (!acc[year]) {
+      acc[year] = [];
+    }
+    acc[year].push(cur);
+    return acc;
+  }, {} as Record<string, any[]>);
+  const sortedEntries = Object.entries(tabsSplitByYear).sort((a, b) => Number(b[0]) - Number(a[0]));
+
+  return (
+    <div className={styles.wrapper}>
+      {sortedEntries.map(([year, timelines]) => (
+        <section key={year} className={styles.section}>
+          <h3 className={styles.year}>{year}</h3>
+          <TimelineList timelines={timelines} />
+        </section>
+      ))}
+    </div>
+  );
 };
