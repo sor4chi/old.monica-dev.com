019f2592847384302836799bd55a73e3cbe750de
sor4chi <sorakawa0218@gmail.com>
2023-04-27 08:17:14 +0900

    style(ui): add margin to tab content wrapper and improve readability of code

--- client/src/ui/foundation/tab/tab-content.css.ts
+++ client/src/ui/foundation/tab/tab-content.css.ts
@@ -3,6 +3,7 @@ import { style } from '@vanilla-extract/css';
 export const tabContentWrapper = style({
   width: '100%',
   overflow: 'hidden',
+  margin: '1rem auto',
 });
 
 export const TAB_CONTENT_GAP = '1rem';
