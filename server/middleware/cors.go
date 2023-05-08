93fee57ca5bc52c91237d83841e879202e703162
sor4chi <sorakawa0218@gmail.com>
2023-05-08 11:08:26 +0900

    chore(dep): update dependencies and fix cors

--- server/middleware/cors.go
+++ server/middleware/cors.go
@@ -2,20 +2,17 @@ package middleware
 
 import (
 	"net/http"
-	"regexp"
 
 	"github.com/sor4chi/portfolio-blog/server/util"
 )
 
 var (
-	ALLOW_ORIGIN_REGEX = util.GetEnv("ALLOW_ORIGIN_REGEX", "^http://localhost:3000$")
+	ALLOW_ORIGIN = util.GetEnv("ALLOW_ORIGIN", "http://localhost:3000")
 )
 
 func CorsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if regexp.MustCompile(ALLOW_ORIGIN_REGEX).MatchString(r.Header.Get("Origin")) {
-			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-		}
+		w.Header().Set("Access-Control-Allow-Origin", ALLOW_ORIGIN)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "accept, content-type, content-length, accept-encoding, x-csrf-token, authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
