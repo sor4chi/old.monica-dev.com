45c550cdf98cc3c1169f05d7c456d456a1392fba
sor4chi <sorakawa0218@gmail.com>
2023-06-23 08:03:05 +0900

    chore: add and configure bundle analyzer

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,12 @@
+const createBundleAnalyzerPlugin = require('@next/bundle-analyzer');
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 
 const { serverEnv: _ } = require('./src/env/server');
 
 const withVanillaExtract = createVanillaExtractPlugin();
+const withBundleAnalyzer = createBundleAnalyzerPlugin({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -26,4 +30,4 @@ const nextConfig = {
   ],
 };
 
-module.exports = withVanillaExtract(nextConfig);
+module.exports = withBundleAnalyzer(withVanillaExtract(nextConfig));
