7aac7df287e73aaf17308286dae20419fb3f48e1
sor4chi <sorakawa0218@gmail.com>
2023-07-15 16:00:05 +0900

    fix: add href to external mdx links, scale link icon

--- apps/new_monica-dev.com/src/components/mdx/Link/Link.tsx
+++ apps/new_monica-dev.com/src/components/mdx/Link/Link.tsx
@@ -4,15 +4,16 @@ import { ArrowUpRight } from 'react-feather';
 import { styles } from './Link.css';
 
 import { TransitionLink } from '@/components/logical/TransitionLink';
+import { vars } from '@/styles/theme.css';
 
 export const Link = ({ children, href, ...props }: ComponentPropsWithoutRef<'a'>) => {
   const isExternal = href?.startsWith('http');
 
   if (isExternal) {
     return (
-      <a className={styles.link} target="_blank" rel="noopener noreferrer" {...props}>
+      <a className={styles.link} target="_blank" rel="noopener noreferrer" href={href} {...props}>
         {children}
-        <ArrowUpRight strokeWidth={1.5} size="1em" className={styles.externalLinkIcon} />
+        <ArrowUpRight strokeWidth={1.5} size={vars.font.size.xl} className={styles.externalLinkIcon} />
       </a>
     );
   }
